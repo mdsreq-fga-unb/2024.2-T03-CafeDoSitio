5814609fffc7d1b32f8c0807a4dd0ae26c65a40c
DanielRogs <danielrocha.dr142@gmail.com>
2024-12-04 03:01:53 -0300

    feat: fazendo pagina de administracao

---
+++ apps/frontendAdmin/src/components/InputText/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InputZone = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: 'Myriad Pro', sans-serif;
+`;
+
+export const Input = styled.input`
+  font-family: 'Myriad Pro', sans-serif;
+  align-items: center;
+  width: 24rem;
+  height: 1.5rem;
+  padding: 8px 12px;
+  border: 2px solid #006343;
+  border-radius: 10px;
+`;
