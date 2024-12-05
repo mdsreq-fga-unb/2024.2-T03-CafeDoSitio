b478c88015fdec4245bb2086567462737feeec90
DanielRogs <danielrocha.dr142@gmail.com>
2024-12-05 00:52:42 -0300

    fix: arrumando build

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { HashRouter as Router, Route, Routes} from 'react-router-dom';
 import ContactPage from './pages/ContactPage'; 
 
 const App = () => {
