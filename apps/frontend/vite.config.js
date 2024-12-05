557cbcd6665579836b4436585030e5c7f7ccc961
DanielRogs <danielrocha.dr142@gmail.com>
2024-12-05 01:17:57 -0300

    fix: arrumando build para Netlify

--- apps/frontend/vite.config.js
+++ apps/frontend/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    target: 'esnext', // Gera código compatível com módulos modernos
+    modulePreload: true,
+  },
   server: {
     port: 3000,
   }
