e79a46bea573e0a5146df22ca799a10f3141d3c9
DanielRogs <danielrocha.dr142@gmail.com>
2024-12-05 01:40:14 -0300

    fix: arrumando build para Netlify

--- apps/frontend/src/components/Footer/index.jsx
+++ apps/frontend/src/components/Footer/index.jsx
@@ -8,7 +8,7 @@ function Footer() {
       <Conteudo>
 
         <ParteSuperior>
-          <img src="./favicon.png" className="logo-img"/>
+          <img src="./assets/FamiliaDoSitioBorda.png" className="logo-img"/>
           <Divisao1>
             <div className="info1">
               <ul className="institucional list">
