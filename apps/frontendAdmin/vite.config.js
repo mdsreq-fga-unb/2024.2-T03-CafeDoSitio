47ac4def9238aa221512a55605c38eb055fdbd88
DanielRogs <danielrocha.dr142@gmail.com>
2024-12-09 01:28:46 -0300

    chore: configurando build

--- apps/frontendAdmin/vite.config.js
+++ apps/frontendAdmin/vite.config.js
@@ -4,8 +4,12 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
   plugins: [react()],
+  base: './',
+  build: {
+    target: 'esnext', // Gera código compatível com módulos modernos
+    modulePreload: true,
+  },
   server: {
     port: 3001,
   }
