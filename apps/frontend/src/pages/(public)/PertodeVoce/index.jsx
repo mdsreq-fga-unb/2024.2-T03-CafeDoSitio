d0a5b8566ec7f824362e3ec9ec6490aff8b854d0
arthur-suares <arthur.lantr@gmail.com>
2025-01-16 19:15:39 -0300

    feat: recolhendo localizacao do usuario

--- apps/frontend/src/pages/(public)/PertodeVoce/index.jsx
+++ apps/frontend/src/pages/(public)/PertodeVoce/index.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Paginacao from '../../../components/Paginacao';
 import {
   Space
 } from "./styled";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const PertodeVoce = () => {
-  return (
-    <>
-    <Paginacao text={"Família do sítio > Perto de você"}/>
-    <Space />
-    </>
-  );
+  const [location, setLocation] = useState(null);
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation([position.coords.latitude, position.coords.longitude]);
+        },
+        (error) => {
+          console.error("Erro ao obter localização:", error);
+        }
+      );
+    } else {
+      console.log("Geolocalização não suportada.");
+    }
+  }, []);
+
+  return (
+    <section>
+    <Paginacao text={"Família do sítio > Perto de você"}/>
+    <Space/>
+    <div>
+      {location ? (
+        <p>Posição do usuário: Latitude = {location[0]}, Longitude = {location[1]}</p>
+        
+      ) : (
+        <p>Obtendo localização...</p>
+      )}
+      
+    </div>
+    </section>
+  );
 };
 
 export default PertodeVoce;
