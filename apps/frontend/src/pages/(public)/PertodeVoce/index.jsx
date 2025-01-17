eb2760108ae78f6d31107353b27f6e03bba35e21
arthur-suares <arthur.lantr@gmail.com>
2025-01-17 12:09:51 -0300

    fix: tela em branco ao tentar obter localizacao

--- apps/frontend/src/pages/(public)/PertodeVoce/index.jsx
+++ apps/frontend/src/pages/(public)/PertodeVoce/index.jsx
@@ -1,43 +1,51 @@
 import React, { useState, useEffect } from "react";
-import Paginacao from '../../../components/Paginacao';
-import {
-  Space
-} from "./styled";
+import Paginacao from "../../../components/Paginacao";
+import { Space } from "./styled";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 
 const PertodeVoce = () => {
-  const [location, setLocation] = useState(null);
+  const [location, setLocation] = useState({ lat: null, lng: null });
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    if (navigator.geolocation) {
+    //Esse if verifica se a api "geolocation" é suportada pelo navegador
+    if ("geolocation" in navigator) {
+
+      /*
+        A função navigator.geolocation.getCurrentPosition() mostra automaticamente na tela
+        o pop up pra pedir permissão da localização. Essa função segue o curso de ação de 
+        position se der tudo certo, ou fluxo de err se algum erro acontecer quando tentar 
+        pegar a localização do usuário
+      */
       navigator.geolocation.getCurrentPosition(
         (position) => {
-          setLocation([position.coords.latitude, position.coords.longitude]);
+          setLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
         },
-        (error) => {
-          console.error("Erro ao obter localização:", error);
+        (err) => {
+          setError("Erro ao obter localização: " + err.message);        
         }
       );
     } else {
-      console.log("Geolocalização não suportada.");
+      setError("Geolocalização não é suportada por este navegador.");
     }
   }, []);
 
   return (
-    <section>
-    <Paginacao text={"Família do sítio > Perto de você"}/>
-    <Space/>
-    <div>
-      {location ? (
-        <p>Posição do usuário: Latitude = {location[0]}, Longitude = {location[1]}</p>
-        
+    <>
+      <Paginacao text={"Família do sítio > Perto de você"} />
+      <Space />
+      {error ? (
+        <p>{error}</p>
+      ) : location.lat && location.lng ? (
+        <h2>Latitude {location.lat}, Longitude {location.lng} </h2>
       ) : (
-        <p>Obtendo localização...</p>
+        <p>Obtendo sua localização...</p>
       )}
-      
-    </div>
-    </section>
+    </>
   );
 };
 
