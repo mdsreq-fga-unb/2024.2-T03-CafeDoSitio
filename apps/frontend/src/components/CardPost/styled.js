0051c1a6deb03a6c5f42e5326d0ce042c832d0ff
anawcarol <122827734+anawcarol@users.noreply.github.com>
2025-02-05 20:57:20 -0300

    feat: realizando componente postblog

---
+++ apps/frontend/src/components/CardPost/styled.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+
+export const Section1 = styled.section`
+  .details {
+    display: flex;
+    flex-direction: row; 
+    align-items: center;
+    gap: 10px;
+    max-width: 70%;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+      padding: 20px;
+      max-width: 1200px;
+      margin: 0 auto;
+
+      h1 {
+        font-size: 1.5rem;
+        font-family: "Myriad Pro", sans-serif;
+        color: #2b674b;
+        margin-block: 20px;
+        padding: 0;
+      }
+
+      .categoria {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 70px;
+        height: 30px;
+        background-color: #fb8b22;
+        border-radius: 10px;
+
+        p {
+          font-size: 0.8rem;
+          color: #fff;
+          margin: 0;
+          padding: 0;
+        }
+      }
+    }
+  }
+
+  .linha-botao {
+    height: 2px;
+    width: 70%; 
+    background-color: #2b674b;
+    margin-block: 20px;
+  }
+`;
+
+export const StyledButton = styled.button`
+  padding: 0.8rem 1.5rem;
+  font-size: 1rem;
+  color: white;
+  background-color: #006343;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  width: 170px;
+  margin-top: 1px;
+
+  &:hover {
+    background-color: #004c2e;
+  }
+`;
