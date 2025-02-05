6010e9ee964ef25f585eff1d34893bc8fbb1ecd1
arthur-suares <arthur.lantr@gmail.com>
2025-02-05 16:54:19 -0300

    feat: mapa leaflet adicionado ao componente com sucesso

--- apps/frontend/src/components/PertodeVoce/index.jsx
+++ apps/frontend/src/components/PertodeVoce/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import Paginacao from "../Paginacao";
 import { 
   Form,
@@ -7,14 +7,15 @@ import {
   Title,
   Button
 } from "./styled";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet"; // Alterei para 'L', pois essa é a convenção do Leaflet
+
 
 function PertodeVoce()  {
   const [location, setLocation] = useState({ lat: null, lng: null });
   const [error, setError] = useState(null);
 
-
+  
   const handleGetLocation = () => {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(
@@ -33,8 +34,24 @@ function PertodeVoce()  {
     }
   };
 
+  const mapRef = useRef(null);
+  const mapContainerRef = useRef(null);
+  
+  
+  useEffect(() => {
+    if (mapRef.current) return; // Evita reinicialização do mapa
+
+    mapRef.current = L.map(mapContainerRef.current).setView([-15.7801, -47.9292], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(mapRef.current);
+  }, []);
+
   return (
     <>
+      <div id="map" ref={mapContainerRef} style={{ height: "500px", width: "100%" }} />
       <Form>
       <Title>Perto de Você</Title>
         <Label htmlFor="name" style={{color: "#006343"}}>Onde você quer encontrar a Família do Sítio?</Label>
