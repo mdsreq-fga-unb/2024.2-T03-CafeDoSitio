81f681516b349cfe8ff63ef9ceb63b22cfaec9b8
anawcarol <122827734+anawcarol@users.noreply.github.com>
2025-02-05 20:22:09 -0300

    feat: adicionando posts do blog

--- apps/frontend/src/pages/(public)/BlogPage/index.jsx
+++ apps/frontend/src/pages/(public)/BlogPage/index.jsx
@@ -1,41 +1,62 @@
 import React from "react";
 import Paginacao from '../../../components/Paginacao';
-import BlogInfo from '../../../assets/blog_info.png'
-
+import BlogInfo from '../../../assets/blog_info.png';
+import BannerBlog from '../../../assets/banner_blog.png';
 
 import {
   Space,
   Section1,
   Section2,
+  StyledButton as Button
 } from "./styled";
 
 const BlogPage = () => {
   return (
     <>
-    <Paginacao text={"Família do sítio > Blog"}/>
-    <Space />
+      <Paginacao text={"Família do sítio > Blog"} />
+      <Space />
 
-    <Section1>
-      <h1>Nosso Blog</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis ab magni amet exercitationem neque, error quos expedita nemo explicabo laborum architecto odit eum ipsa quod libero minus magnam ut nulla.</p>
-      <div className="image-container">
-        <img src={BlogInfo}></img>
-        <div className="text-container">
-          <h2>Aqui você encontrar curiosidades, informações e receitas para os amantes de café!</h2>
+      <Section1>
+        <h1>Nosso Blog</h1>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+          Perspiciatis ab magni amet exercitationem neque, error quos expedita
+          nemo explicabo laborum architecto odit eum ipsa quod libero minus
+          magnam ut nulla.
+        </p>
+        <div className="image-container">
+          <img src={BlogInfo} alt="Blog Info" />
+          <div className="text-container">
+            <h2>
+              Aqui você encontra curiosidades, informações e receitas para os
+              amantes de café!
+            </h2>
+          </div>
         </div>
-      </div>
+        <div className="linha" />
+      </Section1>
 
-      <div className="linha"/>
-    </Section1>
+      <Section2>
+        <div className="container">
+          <h1>Últimas publicações</h1>
+        </div>
 
-    <Section2>
-      <div className="container">
-        <h1>Últimas publições</h1>
-      </div>
+        <div className="details">
+          <img src={BannerBlog} alt="Banner Blog" />
+          <div className="content">
+            <h1>O pretinho básico mais versátil da gastronomia</h1>
+            <div className="categoria">
+              <p>categoria</p>
+            </div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt animi autem inventore, eius vero sapiente quod laboriosam facere maxime quos, excepturi corrupti tenetur quasi sint. Eos veritatis velit similique nam?
+            </p>
+          </div>
+        </div>
 
-    </Section2>
-    
-   
+        <Button>Ler Mais...</Button>
+        <div className="linha-botao" /> 
+      </Section2>
     </>
   );
 };
--- apps/frontend/src/pages/(public)/BlogPage/styled.js
+++ apps/frontend/src/pages/(public)/BlogPage/styled.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+// Espaçamento genérico
 export const Space = styled.div`
   height: 68px;
 `;
@@ -23,33 +24,28 @@ export const Section1 = styled.section`
   }
 
   .image-container {
+    position: relative;
     height: 100%;
     width: 100%;
 
     img {
-      height: 100%;
-      width: 100%;
+      max-width: 100%;
+      height: auto;
       object-fit: cover;
     }
 
     .text-container {
-      width: 100%;
-      left: 0;
-      display: flex;
-      flex-direction: row;
-      justify-content: end;
-      left: 0;
-      padding-inline: 120px;
       position: absolute;
-      margin-top: -220px;
+      bottom: 20px;
+      right: 20px;
+      max-width: 560px;
+      text-align: right;
 
       h2 {
         font-size: 2.4rem;
         font-family: "Myriad Pro", sans-serif;
         color: #fff;
-        text-align: right;
-        max-width: 560px;
-        position: absolute;
+        margin: 0;
       }
     }
   }
@@ -63,8 +59,13 @@ export const Section1 = styled.section`
 
   @media (max-width: 900px) {
     .text-container {
+      max-width: 80%;
+      right: 10px;
+      bottom: 10px;
+
       h2 {
         font-size: 1.5rem;
+        text-align: center;
       }
     }
   }
@@ -76,20 +77,94 @@ export const Section2 = styled.section`
   gap: 10px;
 
   .container {
-    
     width: 100%;
     background-color: #2b674b;
     margin-block: 40px;
-    padding: 8px;
+    padding: 9px;
+    display: flex;
+    justify-content: left;
+    align-items: center;
+    height: 50px;
+    border-radius: 10px;
 
     h1 {
-      font-size: 2.5rem;
+      font-size: 2rem;
       font-family: "Impact", sans-serif;
       color: #fff;
-      margin: 0px;
-
-      
-      
+      margin: 0;
+      padding: 0;
     }
+  }
+
+  .details {
+    display: flex;
+    flex-direction: row; 
+    align-items: center;
+    gap: 10px;
+    max-width: 70%;
+
+    img {
+      width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+
+    .content {
+      display: flex;
+      flex-direction: column;
+      gap: 2px;
+      padding: 20px;
+      max-width: 1200px;
+      margin: 0 auto;
+
+      h1 {
+        font-size: 1.5rem;
+        font-family: "Myriad Pro", sans-serif;
+        color: #2b674b;
+        margin-block: 20px;
+        padding: 0;
+      }
+
+      .categoria {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 70px;
+        height: 30px;
+        background-color: #fb8b22;
+        border-radius: 10px;
+
+        p {
+          font-size: 0.8rem;
+          color: #fff;
+          margin: 0;
+          padding: 0;
+        }
+      }
+    }
+  }
+
+  .linha-botao {
+    height: 2px;
+    width: 70%; 
+    background-color: #2b674b;
+    margin-block: 20px;
+  }
+`;
+
+export const StyledButton = styled.button`
+  padding: 0.8rem 1.5rem;
+  font-size: 1rem;
+  color: white;
+  background-color: #006343;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  width: 170px;
+  margin-top: 1px;
+
+  &:hover {
+    background-color: #004c2e;
   }
 `;
