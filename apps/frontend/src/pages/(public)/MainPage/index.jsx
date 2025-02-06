6ff97f3ee9c4183bbdf681b58347321303f65492
Manuella Valadares <manuella.valadares@hotmail.com>
2025-02-06 14:01:33 -0300

    feat: primeira versão da section 1

--- apps/frontend/src/pages/(public)/MainPage/index.jsx
+++ apps/frontend/src/pages/(public)/MainPage/index.jsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { Container, Section1, Space } from "./styled";
+import { Container, Section1, Space, Card } from "./styled";
 import CapaPagina from "../../../assets/Capa.jpg";
-
-import inicial2 from "../../../assets/inicial2.jpg"
 
 const MainPage = () => {
   return (
     <div className="app">
       <Container>
         <img src={CapaPagina} alt="CapaCafé" className="img"/>
-
         <Section1>
-          
+          <Card>
             <h1>Conheça a nossa família, do Sítio para a sua casa</h1>
-          
-          
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod
+              tincidunt ut laoreet dolore magna aliquam erat.
+            </p>
+          </Card>
         </Section1>
 
         
--- apps/frontend/src/pages/(public)/MainPage/styled.js
+++ apps/frontend/src/pages/(public)/MainPage/styled.js
@@ -3,12 +3,12 @@ import inicial1 from "../../../assets/inicial1.jpg"
 
 export const Container = styled.div`
   z-index: -1;
+  flex: 1;
   position: absolute;
   top: 0;
   left: 0;
   display: flex;
   flex-direction: column;
-  align-items: center;
 
   img{
     width: 100%;
@@ -16,18 +16,44 @@ export const Container = styled.div`
   }
 `;
 
+
+
 export const Space = styled.div`
 height: 1000px;
 `;
 
 export const Section1 = styled.section`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  height: 100%;
   background-image: url(${inicial1});
-  object-fit: cover;
+  display: flex;
+  justify-content: flex-start;
+  padding: 40px;
+  padding-left: 0;
+`;
 
+export const Card = styled.div`
+  
+  color: #333;
+  display: flex;
+  flex-direction: column;
+  align-items: left;
+  justify-content: space-between;
+  width: 40vw;
+  height: 100%;
+  background-color: white;
+  object-fit: cover;
+  border-radius: 0 20px 20px 0;
+
+  h1 {
+    font-size: 4rem;
+    font-family: impact;
+    margin-bottom: 20px;
+    color: #2B674B ;
+  }
+
+  p {
+    font-size: 1.2rem;
+    color: #000;
+    font-weight: 600;
+
+  }
 `;
