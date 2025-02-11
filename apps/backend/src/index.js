ea1c873db12856647bfea4f0de75b41c1ee8269b
DanielRogs <danielrocha.dr142@gmail.com>
2025-02-11 00:26:46 -0300

    fix: corrigindo cors

--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -13,7 +13,8 @@ const app = express();
 
 const allowedOrigins = [
   'http://localhost:3000',
-  'http://localhost:3001'
+  'http://localhost:3001',
+  'https://cafedositio.netlify.app'
 ];
 
 const corsOptionsDelegate = function(req, callback) {
